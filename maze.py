ebd7e91bd278a14f79e3acd002d08077f46ef1be
Fluffycloud123 <34330980+Fluffycloud123@users.noreply.github.com>
2020-02-08 12:54:33 -0800

    experimenting

--- maze.py
+++ maze.py
@@ -1,34 +1,24 @@
 import os.path
-import pygame
+import pygame, thorpy
 
 running = True
-
 pygame.init()
+screenSize = (1000, 600)
+
+pygame.display.set_caption("Maze Generator")
+pygame.display.set_icon(scaledIcon)
+display = pygame.display.set_mode(screenSize)
 clock = pygame.time.Clock()
-display = pygame.display.set_mode((1000, 600))
-font = pygame.font.Font(None, 24)
 
-generateButton = pygame.Rect(0, 0, 100, 60)
-
-def drawButton(rect):
-    pygame.draw.rect(display, (100, 100, 100), rect)
-    text = font.render("Generate", True, (0, 0, 0))
-    display.blit(text, (0, 0))
 
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mousePos = event.pos
-                if generateButton.collidepoint(mousePos):
-                    print("button pressed!")
-         
+
     display.fill((255, 255, 255))
-    drawButton(generateButton)
-    
     pygame.display.update()
-    clock.tick(60)
 
 pygame.quit()
 quit()
